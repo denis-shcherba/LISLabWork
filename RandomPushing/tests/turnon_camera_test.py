45bc6e0e7799c5ae9d3796512d1385290596f811
sendeniz <denizsen@protonmail.com>
2023-09-07 15:04:30 +0200

    ddddddd

--- RandomPushing/tests/turnon_camera_test.py
+++ RandomPushing/tests/turnon_camera_test.py
@@ -4,6 +4,8 @@ import time
 import matplotlib.pyplot as plt
 import cv2
 
+ON_TRUE = False
+
 C = ry.Config()
 C.addFile(ry.raiPath('../rai-robotModels/scenarios/pandaSingle.g'))
 
@@ -15,24 +17,33 @@ obj.setMass(.1)
 obj.setContact(True)
 C.view()
 
-bot = ry.BotOp(C, False)
+bot = ry.BotOp(C, ON_TRUE)
 bot.home(C)
+c = 0 
+while True:
+    c += 1
+    bot.sync(C, .1)
+    if bot.getKeyPressed()==ord('e'):
+       break
+    rgb, depth, points = bot.getImageDepthPcl('cameraWrist', False)
+    
+    if c % 100 == 0:
+        fig = plt.figure(figsize=(10,5))
+        axs = fig.subplots(1, 2)
+        axs[0].imshow(rgb)
+        axs[1].matshow(depth)
+        plt.show()
 
-rgb, depth = bot.getImageAndDepth("cameraWrist")
+        # If camera is not attached, we should get black pixels 
+        total_pixel_sum = np.sum(rgb)
 
-#fig = plt.figure(figsize=(10,5))
-#axs = fig.subplots(1, 2)
-#axs[0].imshow(rgb)
-#axs[1].matshow(depth)
-#plt.show()
+        # Check if the total sum is zero (indicating a black image)
+        if total_pixel_sum == 0:
+            print("Camera records a black image. Check light and camera sensor. ")
+        elif total_pixel_sum == (255 * rgb.shape[2]) * rgb.shape[0] * rgb.shape[1]:
+            print("Camera records a white image. Check light and camera sensor. ")
+        else:
+            print("Camera working.")
 
-# If camera is not attached, we should get black pixels 
-total_pixel_sum = np.sum(rgb)
 
-# Check if the total sum is zero (indicating a black image)
-if total_pixel_sum == 0:
-    print("Camera records a black image. Check light and camera sensor. ")
-elif total_pixel_sum == (255 * rgb.shape[2]) * rgb.shape[0] * rgb.shape[1]:
-    print("Camera records a white image. Check light and camera sensor. ")
-else:
-    print("Camera working.")
+
