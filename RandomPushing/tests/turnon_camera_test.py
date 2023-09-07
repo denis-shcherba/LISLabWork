525598b1ac2865f96602298bc41a6a504c777193
sendeniz <denizsen@protonmail.com>
2023-09-07 14:48:02 +0200

    small camera test added

---
+++ RandomPushing/tests/turnon_camera_test.py
@@ -0,0 +1,38 @@
+from robotic import ry
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+import cv2
+
+C = ry.Config()
+C.addFile(ry.raiPath('../rai-robotModels/scenarios/pandaSingle.g'))
+
+obj = C.addFrame('obj')
+obj.setPose('t(0. 0.1 0.8)')
+obj.setShape(ry.ST.ssBox, size=[.05,.05,.05,.005])
+obj.setColor([1,.0,0])
+obj.setMass(.1)
+obj.setContact(True)
+C.view()
+
+bot = ry.BotOp(C, False)
+bot.home(C)
+
+rgb, depth = bot.getImageAndDepth("cameraWrist")
+
+fig = plt.figure(figsize=(10,5))
+axs = fig.subplots(1, 2)
+axs[0].imshow(rgb)
+axs[1].matshow(depth)
+plt.show()
+
+# If camera is not attached, we should get black pixels 
+total_pixel_sum = np.sum(rgb)
+
+# Check if the total sum is zero (indicating a black image)
+if total_pixel_sum == 0:
+    print("Camera records a black image. Check light and camera sensor. ")
+elif total_pixel_sum == (255 * rgb.shape[2]) * rgb.shape[0] * rgb.shape[1]:
+    print("Camera records a white image. Check light and camera sensor. ")
+else:
+    print("Camera working.")
