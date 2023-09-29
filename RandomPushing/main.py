f2edfd4e25e569bb7e59ea9187ba2feebd993a8d
Eckart Cobo Briesewitz <eck4art@gmail.com>
2023-09-29 11:36:22 +0200

    big code cleanup

--- RandomPushing/main.py
+++ RandomPushing/main.py
@@ -2,18 +2,19 @@ from robotic import ry
 import json
 import numpy as np
 from config import setup_config, startup_robot
-from random_paths import generate_waypointsv2, compute_motion, run_waypoints_one_by_one
+from random_paths import compute_motion, run_waypoints_one_by_one
 from visual import getObject, point2obj
 from arena import *
 
 
-WAYPOINTS = 6
 INITIAL_OBJ_POS = [-.5, 0, .69]
 DEBUG = False
 OBJ_HEIGHT = .08
 
 INR = None
 OTR = .3
+
+ARENA_TYPE = "CIRCLE"
 
 ON_REAL = False
 
@@ -26,7 +27,7 @@ if __name__ == "__main__":
     verbose = 0
 
     #-- define a configuration
-    C = setup_config(WAYPOINTS, INITIAL_OBJ_POS, ON_REAL)
+    C = setup_config(INITIAL_OBJ_POS, ON_REAL)
     if DEBUG:
         key = C.view(verbose>0, 'happy with the config?')
         print('key pressed: ', chr(key)) #use this for basic interaction, e.g. aborting the program
@@ -38,9 +39,9 @@ if __name__ == "__main__":
     data = []
     obj_pos = INITIAL_OBJ_POS
 
-    # first input to plotArena hardcodes radius pos x,y,z
-    circArenaInOut = CircularArena(C=C, middleP=robot_pos, innerR=INR, outerR=OTR)
-    circArenaInOut.plotArena()
+    # Generate Arena
+    arena = CircularArena(middleP=robot_pos, innerR=INR, outerR=OTR)
+    arena.plotArena(C)
 
     # Point towards set initial object position
     point2obj(bot, C, np.array(obj_pos))
@@ -57,9 +58,10 @@ if __name__ == "__main__":
         bot.home(C)
 
         #-- compute a motion (debug this inside the method)
-        way_start, way_end, _, _, success = generate_waypointsv2(C, obj_pos, obj_width=.3, robot_pos=robot_pos, inner_rad=INR, outer_rad=OTR, waypoints=WAYPOINTS)
+        way_start, way_end, _, _, success = arena.generate_waypoints(obj_pos, obj_width=.3, ry_config=C)
+
         if not success: break
-        path, feasible = compute_motion(C, WAYPOINTS, np.array(way_start) - np.array(way_end), verbose)
+        path, feasible = compute_motion(C, verbose)
         print('returned path shape: ', type(path), path.shape)
 
         #-- now, if we're happy with the motion, send it to the robot
--- RandomPushing/tests/path_gen_test.py
+++ RandomPushing/tests/path_gen_test.py
@@ -2,11 +2,8 @@ import pygame
 import sys
 import numpy as np
 from time import sleep
-from random_paths import generate_waypointsv2, generate_waypoints_rect
+from arena import *
 
-ARENA_TYPE="RECTANGLE"
-
-INNER_CIRCLE = True
 # Initialize Pygame
 pygame.init()
 
@@ -14,6 +11,22 @@ pygame.init()
 screen_width, screen_height = 800, 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Path generation test")
+
+# Define arena
+ARENA_TYPE="RECTANGLE"
+inner_rad=50
+robot_pos = np.array([screen_width*.5, screen_height*.5])
+obj_pos = robot_pos+np.array([screen_width*.1, screen_height*.1])
+
+if ARENA_TYPE == "RECTANGLE":
+    rect_width = 400
+    rect_height = 350
+    middlePcirc = robot_pos-np.array([200, 0])
+    arena = RectangularArena(middleP=robot_pos, width=rect_width, height=rect_height, innerR=inner_rad, middlePCirc=middlePcirc)
+
+elif ARENA_TYPE == "CIRCLE":
+    outer_rad = 250
+    arena = CircularArena(middleP=robot_pos, innerR=inner_rad, outerR=outer_rad)
 
 # Colors
 white = (255, 255, 255)
@@ -33,60 +46,39 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    #screen.fill(white)
     sleep(.2)
-    robot_pos = np.array([screen_width*.5, screen_height*.5])
-    obj_pos = robot_pos+np.array([screen_width*.1, screen_height*.1])
-    
-    if ARENA_TYPE=="CIRCLE":
-        inner_rad=50
-        outer_rad=250
 
-        start, end, intersect0, intersect1, _ = generate_waypointsv2(None, obj_pos, 5, robot_pos=robot_pos, inner_rad=inner_rad, outer_rad=outer_rad, start_distance=20)
-        
-        # Draw stuff
-        pygame.draw.circle(screen, black, (robot_pos[0], robot_pos[1]), inner_rad, 2)
+    start, end, intersect0, intersect1, succ = arena.generate_waypoints(obj_pos, 5, start_distance=20)
+
+    # Draw stuff    
+    if ARENA_TYPE=="CIRCLE":
         pygame.draw.circle(screen, black, (robot_pos[0], robot_pos[1]), outer_rad, 2)
         pygame.draw.circle(screen, red, (obj_pos[0], obj_pos[1]), 7)
-
-        pygame.draw.circle(screen, green, (intersect0[0], intersect0[1]), 7)
-        pygame.draw.circle(screen, green, (intersect1[0], intersect1[1]), 7)
-
-        pygame.draw.circle(screen, salmon, (start[0], start[1]), 7)
-        pygame.draw.circle(screen, magenta, (end[0], end[1]), 7)
-        
-        pygame.draw.line(screen, blue, (intersect0[0], intersect0[1]), (intersect1[0], intersect1[1]), 2)
+        if inner_rad:
+            pygame.draw.circle(screen, black, (robot_pos[0], robot_pos[1]), inner_rad, 2)
 
     elif ARENA_TYPE=="RECTANGLE":
-        rect_width = 500
-        rect_height = 350
         rect_x = robot_pos[0]-1/2*rect_width
         rect_y = robot_pos[1]-1/2*rect_height #minus nach oben lang in y lul
-
-
-
-        # Draw stuff
-        if INNER_CIRCLE:
-            inner_rad=50
-            pygame.draw.circle(screen, black, (robot_pos[0], robot_pos[1]), inner_rad, 2)
-            start, end, intersect0, intersect1, _ = generate_waypoints_rect(None, obj_pos, 5, robot_pos=robot_pos, rect_width=rect_width, rect_height=rect_height, start_distance=20, inner_rad=inner_rad)
-        else:
-            start, end, intersect0, intersect1, _ = generate_waypoints_rect(None, obj_pos, 5, robot_pos=robot_pos, rect_width=rect_width, rect_height=rect_height, start_distance=20)
-
-        
-        pygame.draw.circle(screen, red, (obj_pos[0], obj_pos[1]), 7)
-
-
-        pygame.draw.circle(screen, green, (intersect0[0], intersect0[1]), 7)
-        pygame.draw.circle(screen, green, (intersect1[0], intersect1[1]), 7)
-
-        pygame.draw.circle(screen, salmon, (start[0], start[1]), 7)
-        pygame.draw.circle(screen, magenta, (end[0], end[1]), 7)
-        #pygame.draw.circle(screen, (0, 255, 0), (intersect0[0], intersect0[1]), 7)
-        #pygame.draw.circle(screen, (0, 255, 0), (intersect1[0], intersect1[1]), 7)
         outline_rect = pygame.Rect(rect_x, rect_y, rect_width, rect_height)
         pygame.draw.rect(screen, black,  outline_rect, 2)    
         pygame.draw.line(screen, blue, (intersect0[0], intersect0[1]), (intersect1[0], intersect1[1]), 2)
+        if inner_rad:
+            if len(middlePcirc):
+                pygame.draw.circle(screen, black, (middlePcirc[0], middlePcirc[1]), inner_rad, 2)
+            else:
+                pygame.draw.circle(screen, black, (robot_pos[0], robot_pos[1]), inner_rad, 2)
+
+    if succ:
+        pygame.draw.circle(screen, salmon, (start[0], start[1]), 7)
+        pygame.draw.circle(screen, magenta, (end[0], end[1]), 7)
+
+        pygame.draw.circle(screen, green, (intersect0[0], intersect0[1]), 7)
+        pygame.draw.circle(screen, green, (intersect1[0], intersect1[1]), 7)
+        
+        pygame.draw.line(screen, blue, (intersect0[0], intersect0[1]), (intersect1[0], intersect1[1]), 2)
+
+    pygame.draw.circle(screen, red, (obj_pos[0], obj_pos[1]), 7)
 
     # Update the display
     pygame.display.flip()
